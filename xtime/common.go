992fff0a81f02e77851fe77a697775485509e723
yangguang <yangguang@meican.com>
2021-09-07 21:14:22 +0800

    feat: add xtime

---
+++ xtime/common.go
@@ -0,0 +1,14 @@
+package xtime
+
+const (
+	// HoursPerDay defines how many hours that one day is equal to
+	HoursPerDay = 24
+	// ISO8601Date uses ISO 8601 as a default for parsing and rendering
+	ISO8601Date = "2006-01-02"
+	// DateTime format
+	DateTimePattern = "2006-01-02 15:04:05"
+	// Timestamp in string format using
+	TimestampPattern = "2006-01-02 15:04:05"
+	// Numeric string
+	TimeNumericStrPattern = "20060102150405"
+)
